b777c59dc6bbf23157832052cbc674222632fc44
ChornChhery <chherychorn@gmail.com>
2025-08-20 12:51:23 +0700

    training model

--- app.py
+++ app.py
@@ -1,2 +1,16 @@
 import tensorflow as tf
-print(tf.__version__)
+from tensorflow import keras
+from tensorflow.keras.models import load_model
+
+import streamlit as st
+
+
+model = load_model('Image_Classification.keras')
+
+img = 'banana.jpg'
+
+
+image = tf.keras.utils.load_img(image, target_size=(img_height, img_width))
+img_arr = tf.keras.utils.array_to_img(image)
+img_bat = tf.expand_dims(img_arr, 0)
+
